482ba9a034579868641e84dde45cd5059ba2caeb
longbob <710190609@qq.com>
2018-12-25 00:40:44 +0800

    httpClient implement

---
+++ tests/bin/phpunit.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18-11-14
+ * Time: 上午10:37
+ */
+require_once "./vendor/autoload.php";
+go(function() {
+    require_once "./vendor/bin/phpunit";
+});
