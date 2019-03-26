d9fa4f36cf993422d95669beb3211bd47f63a59f
RunsTp <runstp@163.com>
2019-03-26 13:59:53 +0800

    :art:新增开放平台入口

--- src/WeChat.php
+++ src/WeChat.php
@@ -9,14 +9,17 @@
 namespace EasySwoole\WeChat;
 
 
-use EasySwoole\WeChat\MiniProgram\MiniProgram;
+//use EasySwoole\WeChat\MiniProgram\MiniProgram;
 use EasySwoole\WeChat\OfficialAccount\OfficialAccount;
+use EasySwoole\WeChat\OpenPlatform\OpenPlatform;
 
 
 class WeChat
 {
     private $globalConfig;
     private $officialAccount;
+//    private $miniProgram;
+    private $openPlatform;
 
     function __construct(Config $config = null)
     {
@@ -31,10 +34,13 @@ class WeChat
         return $this->globalConfig;
     }
 
-    function miniProgram():MiniProgram
-    {
-
-    }
+//    function miniProgram():MiniProgram
+//    {
+//        if(!isset($this->miniProgram)){
+//            $this->miniProgram = new MiniProgram($this->globalConfig->miniProgram());
+//        }
+//        return $this->miniProgram;
+//    }
 
     function officialAccount():OfficialAccount
     {
@@ -42,5 +48,13 @@ class WeChat
             $this->officialAccount = new OfficialAccount($this->globalConfig->officialAccount());
         }
         return $this->officialAccount;
+    }
+
+    function openPlatform():OpenPlatform
+    {
+        if(!isset($this->openPlatform)){
+            $this->openPlatform = new OpenPlatform($this->globalConfig->openPlatform());
+        }
+        return $this->openPlatform;
     }
 }
