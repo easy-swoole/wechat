e0b5c03c98d6fc3b6c833cc6e03304343758e472
RunsTp <runstp@163.com>
2019-06-11 18:20:36 +0800

    :bug: 修复iv的类型错误问题

--- src/MiniProgram/Encryptor.php
+++ src/MiniProgram/Encryptor.php
@@ -10,11 +10,11 @@ class Encryptor extends MinProgramBase
      * decryptData
      *
      * @param string $sessionKey
-     * @param int    $iv
+     * @param string    $iv
      * @param string $encryptedData
      * @return string
      */
-    public function decryptData(string $sessionKey, int $iv, string $encryptedData)
+    public function decryptData(string $sessionKey, string $iv, string $encryptedData)
     {
         return openssl_decrypt(base64_decode($encryptedData), "aes-128-cbc", base64_decode($sessionKey), OPENSSL_RAW_DATA, base64_decode($iv));
     }
