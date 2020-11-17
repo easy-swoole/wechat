c7b22de118bed8d1a27d20dc0c7f36e394ed95ba
gaobinzhan <gaobinzhan@gmail.com>
2020-11-17 16:15:36 +0800

    add: wifi

--- src/OfficialAccount/Application.php
+++ src/OfficialAccount/Application.php
@@ -14,19 +14,24 @@ use EasySwoole\WeChat\OfficialAccount\Server\Guard;
  * @package EasySwoole\WeChat\OfficialAccount
  * @property AccessToken $accessToken
  * @property AutoReplay\Client $autoReplay
- * @property Base\Client  $base
- * @property Guard  $server
+ * @property Base\Client $base
+ * @property Guard $server
  */
 class Application extends ServiceContainer
 {
     const Base = 'base';
     const Server = 'server';
     const AutoReplay = 'autoReplay';
+    const Wifi = 'wifi';
+    const WifiCard = 'wifiCard';
+    const WifiDevice = 'wifiDevice';
+    const WifiShop = 'wifShop';
 
     protected $providers = [
         Auth\ServiceProvider::class,
         AutoReplay\ServiceProvider::class,
         Base\ServiceProvider::class,
-        Server\ServiceProvider::class
+        Server\ServiceProvider::class,
+        WiFi\ServiceProvider::class
     ];
 }
---
+++ src/OfficialAccount/WiFi/Client.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace EasySwoole\WeChat\OfficialAccount\WiFi;
+
+
+use EasySwoole\WeChat\Kernel\BaseClient;
+use EasySwoole\WeChat\Kernel\ServiceProviders;
+
+class Client extends BaseClient
+{
+    /**
+     * @param string $beginDate
+     * @param string $endDate
+     * @param int $shopId
+     * @return mixed
+     * @throws \EasySwoole\WeChat\Kernel\Exceptions\HttpException
+     */
+    public function summary(string $beginDate, string $endDate, int $shopId = -1)
+    {
+        $data = [
+            'begin_date' => $beginDate,
+            'end_date' => $endDate,
+            'shop_id' => $shopId
+        ];
+
+        $response = $this->getClient()
+            ->setMethod('POST')
+            ->setBody($this->jsonDataToStream($data))
+            ->send($this->buildUrl(
+                '/bizwifi/statistics/list',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+    /**
+     * @param int $shopId
+     * @param string $ssid
+     * @param int $type
+     * @return mixed
+     * @throws \EasySwoole\WeChat\Kernel\Exceptions\HttpException
+     */
+    public function getQrCodeUrl(int $shopId, string $ssid, int $type = 0)
+    {
+        $data = [
+            'shop_id' => $shopId,
+            'ssid' => $ssid,
+            'img_id' => $type,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod('POST')
+            ->setBody($this->jsonDataToStream($data))
+            ->send($this->buildUrl(
+                '/bizwifi/qrcode/get',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+
+    /**
+     * @param array $data
+     * @return bool
+     * @throws \EasySwoole\WeChat\Kernel\Exceptions\HttpException
+     */
+    public function setFinishPage(array $data)
+    {
+        $response = $this->getClient()
+            ->setMethod('POST')
+            ->setBody($this->jsonDataToStream($data))
+            ->send($this->buildUrl(
+                '/bizwifi/finishpage/set',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        return $this->checkResponse($response);
+    }
+
+
+    /**
+     * @param array $data
+     * @return bool
+     * @throws \EasySwoole\WeChat\Kernel\Exceptions\HttpException
+     */
+    public function setHomePage(array $data)
+    {
+        $response = $this->getClient()
+            ->setMethod('POST')
+            ->setBody($this->jsonDataToStream($data))
+            ->send($this->buildUrl(
+                '/bizwifi/homepage/set',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        return $this->checkResponse($response);
+    }
+}
