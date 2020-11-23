c83363f82c296241ef94841a18409ef2c8ee66cd
= <=>
2020-11-23 10:55:56 +0800

    :art: psr 实现修改为依赖mock

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,15 @@ namespace EasySwoole\WeChat\Tests;
 
 
 use EasySwoole\WeChat\Kernel\Contracts\AccessTokenInterface;
-use EasySwoole\WeChat\Kernel\Psr\Response;
-use EasySwoole\WeChat\Kernel\Psr\Stream;
 use EasySwoole\WeChat\Kernel\ServiceContainer;
 use EasySwoole\WeChat\Kernel\ServiceProviders;
+use EasySwoole\WeChat\Tests\Mock\Message\Response;
+use EasySwoole\WeChat\Tests\Mock\Message\ServerRequest;
+use EasySwoole\WeChat\Tests\Mock\Message\Stream;
+use EasySwoole\WeChat\Tests\Mock\Message\Uri;
 use EasySwoole\WeChat\Tests\Mock\MockHttpClient;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
@@ -37,9 +40,9 @@ class TestCase extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @param callable          $onRequest
+     * @param callable $onRequest
      * @param ResponseInterface $response
-     * @param ServiceContainer  $app
+     * @param ServiceContainer $app
      * @return ServiceContainer
      */
     protected function mockHttpClient(callable $onRequest, ResponseInterface $response, ServiceContainer $app): ServiceContainer
@@ -51,9 +54,9 @@ class TestCase extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @param int        $statusCode
+     * @param int $statusCode
      * @param array|null $body
-     * @param array      $headers
+     * @param array $headers
      * @return ResponseInterface
      */
     protected function buildJsonResponse(int $statusCode, array $body = null, array $headers = []): ResponseInterface
@@ -63,13 +66,33 @@ class TestCase extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @param int    $statusCode
+     * @param int $statusCode
      * @param string $body
-     * @param array  $headers
+     * @param array $headers
      * @return ResponseInterface
      */
     protected function buildResponse(int $statusCode, string $body = "", array $headers = []): ResponseInterface
     {
-        return new Response($statusCode, $headers, new Stream($body));
+        return (new Response($headers, new Stream($body)))->withStatus($statusCode);
+    }
+
+    /**
+     * @param string $method
+     * @param string $uri
+     * @param array $headers
+     * @param string $body
+     * @param string $protocolVersion
+     * @param array $serverParams
+     * @return ServerRequestInterface
+     */
+    protected function buildRequest(
+        $method = 'GET',
+        string $uri = "",
+        array $headers = [],
+        string $body = "",
+        $protocolVersion = '1.1',
+        $serverParams = []): ServerRequestInterface
+    {
+        return new ServerRequest($method, new Uri($uri), $headers, new Stream($body), $protocolVersion, $serverParams);
     }
 }
