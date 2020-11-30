56a6ad28df22b7d3bb874fa69469230cfad39caf
runstp <runstp@163.com>
2020-11-30 23:37:17 +0800

    :sparkles: 实现保存文件

--- src/Kernel/Psr/StreamResponse.php
+++ src/Kernel/Psr/StreamResponse.php
@@ -4,15 +4,63 @@
 namespace EasySwoole\WeChat\Kernel\Psr;
 
 
+use EasySwoole\Utility\File;
+use EasySwoole\Utility\Mime\MimeDetectorException;
+use EasySwoole\Utility\MimeType;
 use EasySwoole\WeChat\Kernel\Contracts\StreamResponseInterface;
+use EasySwoole\WeChat\Kernel\Exceptions\InvalidArgumentException;
+use EasySwoole\WeChat\Kernel\Exceptions\RuntimeException;
 use Psr\Http\Message\StreamInterface;
+use Swoole\Coroutine\System;
 
+/**
+ * Class StreamResponse
+ * @package EasySwoole\WeChat\Kernel\Psr
+ */
 class StreamResponse extends Stream implements StreamResponseInterface
 {
+    /**
+     * @param string $directory
+     * @param string $filename
+     * @param bool $appendSuffix
+     * @return string
+     * @throws InvalidArgumentException
+     * @throws RuntimeException
+     */
     public function save(string $directory, string $filename = '', bool $appendSuffix = true): string
     {
-        // TODO 实现保存文件
-        return '';
+        $directory = rtrim($directory, '/');
+
+        if (!File::createDirectory($directory)) {
+            throw new RuntimeException(sprintf('Directory "%s" was not created', $directory));
+        }
+
+        if (!is_writable($directory)) {
+            throw new InvalidArgumentException(sprintf("'%s' is not writable.", $directory));
+        }
+
+        $contents = $this->__toString();
+
+        if (empty($contents) || '{' === $contents[0]) {
+            throw new RuntimeException('Invalid media response content.');
+        }
+
+        if (empty($filename)) {
+            $filename = md5($contents);
+        }
+
+        if ($appendSuffix && empty(pathinfo($filename, PATHINFO_EXTENSION))) {
+            try {
+                $filename .= '.';
+                $filename .= MimeType::getExtFromStream($this);
+            } catch (MimeDetectorException $e) {
+                throw new RuntimeException($e->getMessage(), $e->getCode(), $e);
+            }
+        }
+
+        $this->writeFile($directory . '/' . $filename, $contents);
+
+        return $filename;
     }
 
     /**
@@ -20,6 +68,8 @@ class StreamResponse extends Stream implements StreamResponseInterface
      * @param string $filename
      * @param bool $appendSuffix
      * @return string
+     * @throws InvalidArgumentException
+     * @throws RuntimeException
      */
     public function saveAs(string $directory, string $filename, bool $appendSuffix = true): string
     {
@@ -32,5 +82,22 @@ class StreamResponse extends Stream implements StreamResponseInterface
     public function getStream(): StreamInterface
     {
         return $this;
+    }
+
+    /**
+     * @param string $filename
+     * @param string $contents
+     * @return mixed
+     * @throws RuntimeException
+     */
+    protected function writeFile(string $filename, string $contents)
+    {
+        $fp = fopen($filename, 'w');
+        $len = System::fwrite($fp, $contents);
+        fclose($fp);
+        if ($len === false) {
+            throw new RuntimeException('Write file error.');
+        }
+        return $len;
     }
 }
