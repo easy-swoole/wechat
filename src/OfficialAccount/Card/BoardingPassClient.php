3421a99b57a7b0f59fbdc8b12267d4ad0a0ba308
gaobinzhan <gaobinzhan@gmail.com>
2020-12-26 23:18:14 +0800

    add: boarding pass client

--- src/OfficialAccount/Card/BoardingPassClient.php
+++ src/OfficialAccount/Card/BoardingPassClient.php
@@ -5,8 +5,19 @@ namespace EasySwoole\WeChat\OfficialAccount\Card;
 
 
 use EasySwoole\WeChat\Kernel\BaseClient;
+use EasySwoole\WeChat\Kernel\ServiceProviders;
 
 class BoardingPassClient extends BaseClient
 {
+    public function checkin(array $params)
+    {
+        $response = $this->getClient()->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/boardingpass/checkin',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
 
+        return $this->checkResponse($response);
+    }
 }
