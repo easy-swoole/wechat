1c2514974b328b1bbb28494f19a4058a4707ee1e
gaobinzhan <gaobinzhan@gmail.com>
2020-12-27 23:03:43 +0800

    add: card code client

--- src/OfficialAccount/Card/CodeClient.php
+++ src/OfficialAccount/Card/CodeClient.php
@@ -5,8 +5,168 @@ namespace EasySwoole\WeChat\OfficialAccount\Card;
 
 
 use EasySwoole\WeChat\Kernel\BaseClient;
+use EasySwoole\WeChat\Kernel\ServiceProviders;
 
 class CodeClient extends BaseClient
 {
+    /**
+     * @param string $cardId
+     * @param array $codes
+     * @return bool
+     * @throws \EasySwoole\WeChat\Kernel\Exceptions\HttpException
+     * @link https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Distributing_Coupons_Vouchers_and_Cards.html
+     */
+    public function deposit(string $cardId, array $codes)
+    {
+        $params = [
+            'card_id' => $cardId,
+            'code' => $codes,
+        ];
 
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/deposit',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        return $this->checkResponse($response);
+    }
+
+    public function getDepositedCount(string $cardId)
+    {
+        $params = [
+            'card_id' => $cardId,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/getdepositcount',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+    public function check(string $cardId, array $codes)
+    {
+        $params = [
+            'card_id' => $cardId,
+            'code' => $codes,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/checkcode',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+    public function get(string $code, string $cardId = '', bool $checkConsume = true)
+    {
+        $params = [
+            'code' => $code,
+            'check_consume' => $checkConsume,
+            'card_id' => $cardId,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/get',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+    public function update(string $code, string $newCode, string $cardId = '')
+    {
+        $params = [
+            'code' => $code,
+            'new_code' => $newCode,
+            'card_id' => $cardId,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/update',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        return $this->checkResponse($response);
+    }
+
+    public function disable(string $code, string $cardId = '')
+    {
+        $params = [
+            'code' => $code,
+            'card_id' => $cardId,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/unavailable',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        return $this->checkResponse($response);
+    }
+
+
+    public function consume(string $code, string $cardId = null)
+    {
+        $params = [
+            'code' => $code,
+        ];
+
+        if (!is_null($cardId)) {
+            $params['card_id'] = $cardId;
+        }
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/consume',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+
+    public function decrypt(string $encryptedCode)
+    {
+        $params = [
+            'encrypt_code' => $encryptedCode,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/code/decrypt',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
 }
