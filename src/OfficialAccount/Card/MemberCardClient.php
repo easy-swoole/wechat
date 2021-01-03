80216ddc8a38ad7f5ddab3cb33381115ef2f152e
gaobinzhan <gaobinzhan@gmail.com>
2021-01-03 20:45:58 +0800

    add: card member card client

--- src/OfficialAccount/Card/MemberCardClient.php
+++ src/OfficialAccount/Card/MemberCardClient.php
@@ -5,8 +5,100 @@ namespace EasySwoole\WeChat\OfficialAccount\Card;
 
 
 use EasySwoole\WeChat\Kernel\BaseClient;
+use EasySwoole\WeChat\Kernel\ServiceProviders;
 
 class MemberCardClient extends BaseClient
 {
+    public function activate(array $info = [])
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($info))
+            ->send($this->buildUrl(
+                '/card/membercard/activate',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
 
+        return $this->checkResponse($response);
+    }
+
+    public function setActivationForm(string $cardId, array $settings)
+    {
+        $params = array_merge(['card_id' => $cardId], $settings);
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/membercard/activateuserform/set',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        return $this->checkResponse($response);
+    }
+
+    public function getUser(string $cardId, string $code)
+    {
+        $params = [
+            'card_id' => $cardId,
+            'code' => $code,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/membercard/userinfo/get',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+    public function updateUser(array $params = [])
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/membercard/updateuser',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+    public function getActivationForm($activateTicket)
+    {
+        $params = [
+            'activate_ticket' => $activateTicket,
+        ];
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/membercard/activatetempinfo/get',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+    public function getActivateUrl(array $params = [])
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/membercard/activate/geturl',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
 }
