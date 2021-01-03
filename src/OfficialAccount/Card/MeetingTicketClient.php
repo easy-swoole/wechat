ab27674f7c667665788243e9a6710c09c62e7ca5
gaobinzhan <gaobinzhan@gmail.com>
2021-01-03 20:40:04 +0800

    add: card meeting ticket client

--- src/OfficialAccount/Card/MeetingTicketClient.php
+++ src/OfficialAccount/Card/MeetingTicketClient.php
@@ -5,8 +5,20 @@ namespace EasySwoole\WeChat\OfficialAccount\Card;
 
 
 use EasySwoole\WeChat\Kernel\BaseClient;
+use EasySwoole\WeChat\Kernel\ServiceProviders;
 
 class MeetingTicketClient extends BaseClient
 {
+    public function updateUser(array $params)
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                '/card/meetingticket/updateuser',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
 
+        return $this->checkResponse($response);
+    }
 }
