20668b6f7f0d2994de8692a2f33f541cb8592c7e
runstp <runstp@163.com>
2021-03-04 01:09:28 +0800

    :sparkles: 完善群发消息API

--- src/OfficialAccount/Broadcasting/Client.php
+++ src/OfficialAccount/Broadcasting/Client.php
@@ -4,13 +4,24 @@
 namespace EasySwoole\WeChat\OfficialAccount\Broadcasting;
 
 
+use BadMethodCallException;
 use EasySwoole\WeChat\Kernel\BaseClient;
 use EasySwoole\WeChat\Kernel\Contracts\MessageInterface;
 use EasySwoole\WeChat\Kernel\Exceptions\HttpException;
+use EasySwoole\WeChat\Kernel\Exceptions\RuntimeException;
+use EasySwoole\WeChat\Kernel\Messages\Card;
+use EasySwoole\WeChat\Kernel\Messages\Image;
+use EasySwoole\WeChat\Kernel\Messages\Media;
+use EasySwoole\WeChat\Kernel\Messages\Text;
 use EasySwoole\WeChat\Kernel\ServiceProviders;
+use function is_array;
+use function is_int;
 
 class Client extends BaseClient
 {
+    public const PREVIEW_BY_OPENID = 'touser';
+    public const PREVIEW_BY_NAME = 'towxname';
+
     /**
      * @param array $message
      * @return array
@@ -32,15 +43,57 @@ class Client extends BaseClient
     }
 
     /**
-     * @param MessageInterface $message
-     * @param null $reception
-     * @return array
+     * @param array $message
+     * @return mixed
      * @throws HttpException
      */
-    public function sendMessage(MessageInterface $message, $reception = null): array
+    public function preview(array $message)
     {
-        // TODO: build message
-        return $this->send($message->transformForJsonRequest());
+        $response = $this->getClient()->setMethod("POST")
+            ->setBody($this->jsonDataToStream($message))
+            ->send($this->buildUrl(
+                '/cgi-bin/message/mass/preview',
+                ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
+            ));
+
+        $this->checkResponse($response, $jsonData);
+        return $jsonData;
+    }
+
+    /**
+     * @param MessageInterface $message
+     * @param null $reception
+     * @param array $attributes
+     * @return array
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function sendMessage(MessageInterface $message, $reception = null, $attributes = []): array
+    {
+        $message = (new MessageBuilder())->message($message)->with($attributes)->toAll();
+
+        if (is_int($reception)) {
+            $message->toTag($reception);
+        } elseif (is_array($reception)) {
+            $message->toUsers($reception);
+        }
+
+        return $this->send($message->build());
+    }
+
+    /**
+     * @param MessageInterface $message
+     * @param string $reception
+     * @param $method
+     * @return mixed
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function previewMessage(MessageInterface $message, string $reception, $method = self::PREVIEW_BY_OPENID)
+    {
+        $message = (new MessageBuilder())->message($message)->buildForPreview($method, $reception);
+
+        return $this->preview($message);
     }
 
     /**
@@ -82,5 +135,181 @@ class Client extends BaseClient
             ));
 
         return $this->checkResponse($response);
+    }
+
+    /**
+     * @param string $message
+     * @param null $reception
+     * @param array $attributes
+     * @return array
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function sendText(string $message, $reception = null, array $attributes = [])
+    {
+        return $this->sendMessage(new Text($message), $reception, $attributes);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param null $reception
+     * @param array $attributes
+     * @return array
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function sendNews(string $mediaId, $reception = null, array $attributes = [])
+    {
+        return $this->sendMessage(new Media($mediaId, Media::MPNEWS), $reception, $attributes);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param null $reception
+     * @param array $attributes
+     * @return array
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function sendVoice(string $mediaId, $reception = null, array $attributes = [])
+    {
+        return $this->sendMessage(new Media($mediaId, Media::VOICE), $reception, $attributes);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param null $reception
+     * @param array $attributes
+     * @return array
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function sendImage(string $mediaId, $reception = null, array $attributes = [])
+    {
+        return $this->sendMessage(new Image($mediaId), $reception, $attributes);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param null $reception
+     * @param array $attributes
+     * @return array
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function sendVideo(string $mediaId, $reception = null, array $attributes = [])
+    {
+        return $this->sendMessage(new Media($mediaId, Media::MPVIDEO), $reception, $attributes);
+    }
+
+    /**
+     * @param string $cardId
+     * @param null $reception
+     * @param array $attributes
+     * @return array
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function sendCard(string $cardId, $reception = null, array $attributes = [])
+    {
+        return $this->sendMessage(new Card($cardId), $reception, $attributes);
+    }
+
+    /**
+     * @param string $message
+     * @param $reception
+     * @param string $method
+     * @return mixed
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function previewText(string $message, $reception, $method = self::PREVIEW_BY_OPENID)
+    {
+        return $this->previewMessage(new Text($message), $reception, $method);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param $reception
+     * @param string $method
+     * @return mixed
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function previewNews(string $mediaId, $reception, $method = self::PREVIEW_BY_OPENID)
+    {
+        return $this->previewMessage(new Media($mediaId, Media::MPNEWS), $reception, $method);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param $reception
+     * @param string $method
+     * @return mixed
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function previewVoice(string $mediaId, $reception, $method = self::PREVIEW_BY_OPENID)
+    {
+        return $this->previewMessage(new Media($mediaId, Media::VOICE), $reception, $method);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param $reception
+     * @param string $method
+     * @return mixed
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function previewImage(string $mediaId, $reception, $method = self::PREVIEW_BY_OPENID)
+    {
+        return $this->previewMessage(new Image($mediaId), $reception, $method);
+    }
+
+    /**
+     * @param string $mediaId
+     * @param $reception
+     * @param string $method
+     * @return mixed
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function previewVideo(string $mediaId, $reception, $method = self::PREVIEW_BY_OPENID)
+    {
+        return $this->previewMessage(new Media($mediaId, Media::MPVIDEO), $reception, $method);
+    }
+
+    /**
+     * @param string $cardId
+     * @param $reception
+     * @param string $method
+     * @return mixed
+     * @throws HttpException
+     * @throws RuntimeException
+     */
+    public function previewCard(string $cardId, $reception, $method = self::PREVIEW_BY_OPENID)
+    {
+        return $this->previewMessage(new Card($cardId), $reception, $method);
+    }
+
+    /**
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        if (strpos($method, 'ByName') > 0) {
+            $method = strstr($method, 'ByName', true);
+
+            if (method_exists($this, $method)) {
+                array_push($args, self::PREVIEW_BY_NAME);
+
+                return $this->$method(...$args);
+            }
+        }
+
+        throw new BadMethodCallException(sprintf('Method %s not exists.', $method));
     }
 }
---
+++ src/OfficialAccount/Broadcasting/MessageBuilder.php
@@ -0,0 +1,135 @@
+<?php
+
+
+namespace EasySwoole\WeChat\OfficialAccount\Broadcasting;
+
+
+use EasySwoole\WeChat\Kernel\Contracts\MessageInterface;
+use EasySwoole\WeChat\Kernel\Exceptions\RuntimeException;
+
+/**
+ * Class MessageBuilder
+ * @package EasySwoole\WeChat\OfficialAccount\Broadcasting
+ */
+class MessageBuilder
+{
+    /**
+     * @var array
+     */
+    protected $to = [];
+
+    /**
+     * @var MessageInterface
+     */
+    protected $message;
+
+    /**
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * @param MessageInterface $message
+     * @return $this
+     */
+    public function message(MessageInterface $message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * @param array $to
+     * @return $this
+     */
+    public function to(array $to)
+    {
+        $this->to = $to;
+
+        return $this;
+    }
+
+    /**
+     * @param int $tagId
+     * @return $this
+     */
+    public function toTag(int $tagId)
+    {
+        $this->to([
+            'filter' => [
+                'is_to_all' => false,
+                'tag_id' => $tagId,
+            ],
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @param array $openidList
+     * @return $this
+     */
+    public function toUsers(array $openidList)
+    {
+        $this->to([
+            'touser' => $openidList,
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function toAll()
+    {
+        $this->to([
+            'filter' => ['is_to_all' => true],
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @param array $attributes
+     * @return $this
+     */
+    public function with(array $attributes)
+    {
+        $this->attributes = $attributes;
+
+        return $this;
+    }
+
+    /**
+     * @param array $prepends
+     * @return array
+     * @throws RuntimeException
+     */
+    public function build(array $prepends = []): array
+    {
+        if (empty($this->message)) {
+            throw new RuntimeException('No message content to send.');
+        }
+
+        $content = $this->message->transformForJsonRequest();
+
+        if (empty($prepends)) {
+            $prepends = $this->to;
+        }
+
+        return array_merge($prepends, $content, $this->attributes);
+    }
+
+    /**
+     * @param string $by
+     * @param string $user
+     * @return array
+     * @throws RuntimeException
+     */
+    public function buildForPreview(string $by, string $user): array
+    {
+        return $this->build([$by => $user]);
+    }
+}
