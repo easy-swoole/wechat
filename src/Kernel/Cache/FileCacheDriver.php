143331855da8c0e09bdf98d801ace6367fa297a6
RunsTp <runstp@163.com>
2021-03-03 11:18:53 +0800

    :bug: fix file cache 没有正确的设置不过期问题

--- src/Kernel/Cache/FileCacheDriver.php
+++ src/Kernel/Cache/FileCacheDriver.php
@@ -15,10 +15,15 @@ class FileCacheDriver implements CacheInterface
      */
     protected $tmpDir;
 
+    /**
+     * FileCacheDriver constructor.
+     * @param string $directory
+     * @throws RuntimeException
+     */
     public function __construct(string $directory)
     {
         if (!is_dir($directory)) {
-            if(!mkdir($directory, 0755, true)){
+            if (!mkdir($directory, 0755, true)) {
                 throw new RuntimeException("create dir {$directory} fail");
             }
         }
@@ -29,28 +34,33 @@ class FileCacheDriver implements CacheInterface
     public function get($key, $default = null)
     {
         $file = $this->key2File($key);
-        if(file_exists($file)){
+        if (file_exists($file)) {
             $data = unserialize(file_get_contents($file));
-            if($data && time() < $data['t']){
+            if ($data && ($data['t'] < 0 || time() < $data['t'])) {
                 return $data['d'];
             }
         }
-        return  $default;
+        return $default;
     }
 
     public function set($key, $value, $ttl = null)
     {
         $data = [
-            'd'=>$value,
-            't'=>$ttl + time()
+            'd' => $value,
+            /** 支持不过期 */
+            't' => is_null($ttl) ? -1 : $ttl + time()
         ];
-        return file_put_contents($this->key2File($key),serialize($data));
+
+        if (file_put_contents($this->key2File($key), serialize($data))) {
+            return true;
+        }
+        return false;
     }
 
     public function delete($key)
     {
         $file = $this->key2File($key);
-        if(file_exists($file)){
+        if (file_exists($file)) {
             unlink($file);
         }
         return true;
@@ -73,7 +83,7 @@ class FileCacheDriver implements CacheInterface
 
     public function deleteMultiple($keys)
     {
-        foreach ($keys as $key){
+        foreach ($keys as $key) {
             $this->delete($key);
         }
     }
@@ -84,10 +94,8 @@ class FileCacheDriver implements CacheInterface
         return file_exists($file);
     }
 
-    private function key2File($key):string
+    private function key2File($key): string
     {
-        return $this->tmpDir.'/'.substr(md5($key),8,16);
+        return $this->tmpDir . "/{$key}";
     }
-
-
 }
