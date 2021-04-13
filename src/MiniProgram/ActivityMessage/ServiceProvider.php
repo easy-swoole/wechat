f06cb514a327d5f9168d31e7a0a9dedc8e90ee54
gaobinzhan <gaobinzhan@gmail.com>
2021-04-13 15:09:11 +0800

    Fix: MiniProgram ActivityMessage ServiceProvider

--- src/MiniProgram/ActivityMessage/ServiceProvider.php
+++ src/MiniProgram/ActivityMessage/ServiceProvider.php
@@ -4,14 +4,14 @@ namespace EasySwoole\WeChat\MiniProgram\ActivityMessage;
 
 
 use EasySwoole\WeChat\MiniProgram\Application;
-use EasySwoole\WeChat\MiniProgram\BaseClient;
 use Pimple\Container;
+use Pimple\ServiceProviderInterface;
 
 /**
  * Class ServiceProvider
  * @package EasySwoole\WeChat\MiniProgram\ActivityMessage
  */
-class ServiceProvider extends BaseClient
+class ServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $app)
     {
