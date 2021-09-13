95a470dd06f076f62b588bee7b7779d87239c244
ysongyang <49271743@qq.com>
2021-09-13 13:35:24 +0800

    提交试用小程序接口

--- src/OpenPlatform/Component/BetaProgram/Client.php
+++ src/OpenPlatform/Component/BetaProgram/Client.php
@@ -64,8 +64,78 @@ class Client extends BaseClient
                 ]
             ));
 
+        return $this->checkResponse($response);
+    }
+
+
+    /**
+     * 修改试用小程序名称
+     * 该接口用于修改试用小程序名称
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/beta_Mini_Programs/fastmodify.html
+     * @param array $params
+     * @return mixed
+     * @throws HttpException
+     */
+    public function setbetaweappnickname(array $params)
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                "/wxa/setbetaweappnickname",
+                [
+                    'access_token' => $this->app[ServiceProviders::AccessToken]->getToken()
+                ]
+            ));
+
+        return $this->checkResponse($response);
+    }
+
+
+    /**
+     * 获取公众号管理员授权
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/verify_beta_Mini_Programs/getmpadminauth.html
+     * @param array $params
+     * @return mixed
+     * @throws HttpException
+     */
+    public function getmpadminauth(array $params)
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                "/wxa/getmpadminauth",
+                [
+                    'access_token' => $this->app[ServiceProviders::AccessToken]->getToken()
+                ]
+            ));
+
         $this->checkResponse($response, $parseData);
         return $parseData;
     }
 
+
+    /**
+     * 复用公众号主体认证小程序
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/verify_beta_Mini_Programs/mpverifybetaweapp.html
+     * @param array $params
+     * @return mixed
+     * @throws HttpException
+     */
+    public function mpverifybetaweapp(array $params)
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                "/wxa/mpverifybetaweapp",
+                [
+                    'access_token' => $this->app[ServiceProviders::AccessToken]->getToken()
+                ]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
 }
