858e44a4cc045c95a43e7d4a689dfcf7abf88957
ysongyang <49271743@qq.com>
2021-09-13 13:23:22 +0800

    commit 试用小程序

---
+++ src/OpenPlatform/Component/BetaProgram/Client.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+/**
+ ***********作者信息**********
+ * Author: ysongyang
+ * Email: 49271743@qq.com
+ * Desc: 文件描述
+ * Date: 2021/9/13 13:22
+ *****************************
+ */
+
+namespace EasySwoole\WeChat\OpenPlatform\Component\BetaProgram;
+
+
+use EasySwoole\WeChat\Kernel\Exceptions\HttpException;
+use EasySwoole\WeChat\Kernel\ServiceProviders;
+use EasySwoole\WeChat\OpenPlatform\BaseClient;
+
+class Client extends BaseClient
+{
+
+    /**
+     * 创建试用小程序
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/beta_Mini_Programs/fastregister.html
+     * @param array $params
+     * @return mixed
+     * @throws HttpException
+     */
+    public function fastregisterbetaweapp(array $params)
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                "/wxa/component/fastregisterbetaweapp",
+                [
+                    'component_access_token' => $this->app[ServiceProviders::AccessToken]->getToken()
+                ]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+
+    /**
+     * 试用小程序快速认证
+     * 该接口用于通过企业法人人脸识别的方式快速将试用小程序进行认证
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/beta_Mini_Programs/fastverify.html
+     * @param array $params
+     * @return mixed
+     * @throws HttpException
+     */
+    public function verifybetaweapp(array $params)
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($params))
+            ->send($this->buildUrl(
+                "/wxa/verifybetaweapp",
+                [
+                    'access_token' => $this->app[ServiceProviders::AccessToken]->getToken()
+                ]
+            ));
+
+        $this->checkResponse($response, $parseData);
+        return $parseData;
+    }
+
+}
