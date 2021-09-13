1f4089c04bf2719c518d92c5ba4f233b13754033
ysongyang <49271743@qq.com>
2021-09-13 13:52:18 +0800

    修复复制公众号主题注册小程序接口的bug;register方法增加参数timeout

--- src/OpenPlatform/Authorizer/OfficialAccount/Account/Client.php
+++ src/OpenPlatform/Authorizer/OfficialAccount/Account/Client.php
@@ -47,16 +47,18 @@ class Client extends BaseClient
     }
 
     /**
-     * doc link: 找不到对应文档
-     *
+     * 从第三方平台跳转至微信公众平台授权注册页面, 授权注册小程序.
+     * doc link:https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/Register_Mini_Programs/fast_registration_of_mini_program.html
      * @param string $ticket
+     * @param float $timeout
      * @return mixed
      * @throws HttpException
      */
-    public function register(string $ticket)
+    public function register(string $ticket, float $timeout = 10)
     {
         $response = $this->getClient()
             ->setMethod("POST")
+            ->setTimeout($timeout)
             ->setBody($this->jsonDataToStream([
                 'ticket' => $ticket,
             ]))->send($this->buildUrl(
