62fc36ec64ed3dfcfb7a9901467170133546f612
XueSi <1592328848@qq.com>
2021-09-15 20:19:24 +0800

    feat:添加开放平台快速注册个人小程序接口

--- src/OpenPlatform/Component/Client.php
+++ src/OpenPlatform/Component/Client.php
@@ -17,7 +17,7 @@ class Client extends BaseClient
     /**
      * 通过法人微信快速创建小程序.
      * 代注册小程序 - 快速创建小程序
-     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Register_Mini_Programs/Fast_Registration_Interface_document.html
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/Register_Mini_Programs/Fast_Registration_Interface_document.html
      *
      * @param array $params
      * @return mixed
@@ -42,7 +42,7 @@ class Client extends BaseClient
     /**
      * 查询创建任务状态.
      * 代注册小程序 - 快速创建小程序 - 查询创建任务状态
-     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Register_Mini_Programs/Fast_Registration_Interface_document.html
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/Register_Mini_Programs/Fast_Registration_Interface_document.html
      *
      * @param string $companyName
      * @param string $legalPersonaWechat
@@ -67,5 +67,66 @@ class Client extends BaseClient
             ));
 
         return $this->checkResponse($response);
+    }
+
+    /**
+     * 快速创建个人小程序 - 创建个人主体小程序
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/Register_Mini_Programs/fastregisterpersonalweapp.html
+     *
+     * @param string $idName
+     * @param string $wxUser
+     * @param string|null $componentPhone
+     * @return mixed
+     * @throws HttpException
+     */
+    public function registerPersonalWeapp(string $idName, string $wxUser, string $componentPhone = null)
+    {
+        $param = [
+            'idname' => $idName,
+            'wxuser' => $wxUser,
+        ];
+
+        if (!is_null($componentPhone)) {
+            $param['component_phone'] = $componentPhone;
+        }
+
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream($param))
+            ->send($this->buildUrl(
+                "/wxa/component/fastregisterpersonalweapp",
+                [
+                    'action' => 'create',
+                    'component_access_token' => $this->app[ServiceProviders::AccessToken]->getToken()
+                ]
+            ));
+
+        $this->checkResponse($response, $data);
+        return $data;
+    }
+
+    /**
+     * 快速创建个人小程序 - 查询创建任务状态
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/Register_Mini_Programs/fastregisterpersonalweapp.html
+     *
+     * @param string $taskId
+     * @return mixed
+     * @throws HttpException
+     */
+    public function getPersonalWeappStatus(string $taskId)
+    {
+        $response = $this->getClient()
+            ->setMethod("POST")
+            ->setBody($this->jsonDataToStream(['taskid' => $taskId]))
+            ->send($this->buildUrl(
+                "/wxa/component/fastregisterpersonalweapp",
+                [
+                    'action' => 'query',
+                    'component_access_token' => $this->app[ServiceProviders::AccessToken]->getToken()
+                ]
+            ));
+
+        $this->checkResponse($response, $data);
+        return $data;
     }
 }
