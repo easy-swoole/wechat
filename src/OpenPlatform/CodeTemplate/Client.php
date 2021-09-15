4199470e754bc8c6ab41edaac1715861cf7b8df0
XueSi <1592328848@qq.com>
2021-09-15 18:57:57 +0800

    fix:the issue #161

--- src/OpenPlatform/CodeTemplate/Client.php
+++ src/OpenPlatform/CodeTemplate/Client.php
@@ -17,7 +17,7 @@ class Client extends BaseClient
     /**
      * 获取草稿箱内的所有临时代码草稿.
      * 代小程序实现业务 - 代码模板库设置 - 获取代码草稿列表
-     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code_template/gettemplatedraftlist.html
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/code_template/gettemplatedraftlist.html
      *
      * @return mixed
      * @throws HttpException
@@ -38,18 +38,20 @@ class Client extends BaseClient
     /**
      * 将草稿箱的草稿选为小程序代码模版.
      * 代小程序实现业务 - 代码模板库设置 - 将草稿添加到代码模板库
-     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code_template/addtotemplate.html
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/code_template/addtotemplate.html
      *
      * @param int $draftId
+     * @param int $templateType
      * @return mixed
      * @throws HttpException
      */
-    public function createFromDraft(int $draftId)
+    public function createFromDraft(int $draftId, int $templateType = 0)
     {
         $response = $this->getClient()
             ->setMethod("POST")
             ->setBody($this->jsonDataToStream([
                 'draft_id' => $draftId,
+                'template_type' => $templateType,
             ]))->send($this->buildUrl(
                 "/wxa/addtotemplate",
                 ['access_token' => $this->app[ServiceProviders::AccessToken]->getToken()]
@@ -61,7 +63,7 @@ class Client extends BaseClient
     /**
      * 获取代码模版库中的所有小程序代码模版.
      * 代小程序实现业务 - 代码模板库设置 - 获取代码模板列表
-     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code_template/gettemplatelist.html
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/code_template/gettemplatelist.html
      *
      * @return mixed
      * @throws HttpException
@@ -83,7 +85,7 @@ class Client extends BaseClient
      * 删除指定小程序代码模版.
      * 获取代码模版库中的所有小程序代码模版.
      * 代小程序实现业务 - 代码模板库设置 - 删除指定代码模板
-     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/Mini_Programs/code_template/deletetemplate.html
+     * doc link: https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/code_template/deletetemplate.html
      *
      * @param $templateId
      * @return mixed
